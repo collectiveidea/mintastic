a2d5038ac3f6704f3a17e2351df1203c36ab987e
Jon Stokes <jon@jonstokes.com>
2016-03-31 15:11:40 -0500

    Stash progress

--- app/assets/javascripts/components/file_upload_input.es6.jsx
+++ app/assets/javascripts/components/file_upload_input.es6.jsx
@@ -6,7 +6,7 @@ class FileUploadInput extends React.Component {
     return (
       <form ref="form" className="upload-form" action={ this.props.form.action } acceptCharset="UTF-8" method="post" onSubmit={ this.handleSubmit }>
         <div class="form-group">
-          <label for={this.props.fieldName}>{this.props.label}</label>
+          <label for={this.props.fieldName}>{this.props.labelText}</label>
           <input type="hidden" name={ this.props.form.csrf_param } value={ this.props.form.csrf_token } />
           <input name={this.props.fieldName} placeholder={this.props.placeHolder} />
         </div>
@@ -17,7 +17,7 @@ class FileUploadInput extends React.Component {
 }
 
 FileUploadInput.propTypes = {
-  label: React.PropTypes.string.isRequired,
+  labelText: React.PropTypes.string.isRequired,
   fieldName: React.PropTypes.string.isRequired,
   placeHolder: React.PropTypes.string.isRequired,
   form: React.PropTypes.node.isRequired
