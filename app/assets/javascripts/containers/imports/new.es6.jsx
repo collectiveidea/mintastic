4dff8bcddb6ae1422bb7a6b1b270740e28131ad0
Jon Stokes <jon@jonstokes.com>
2016-04-01 15:30:57 -0500

    Dropzone works

--- app/assets/javascripts/containers/imports/new.es6.jsx
+++ app/assets/javascripts/containers/imports/new.es6.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Dropzone from 'react-dropzone';
 
 class Import extends React.Component {
+  onDrop (files) {
+    console.log('Received files: ', files);
+  }
+
   render() {
     return (
-      <div>Stuff goes here!</div>
-    );
+      <Dropzone onDrop={this.onDrop}>
+        <div>Try dropping some files here, or click to select files to upload.</div>
+      </Dropzone>
+    )
   }
 }
 
