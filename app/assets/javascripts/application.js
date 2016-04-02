e66edd246b762154c3076c85b45de97162299e88
Jon Stokes <jon@jonstokes.com>
2016-04-02 10:17:58 -0500

    Jquery ujs is already coming in via browserify

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require_self
 //= require react-server
 //= require react_ujs
-//= require jquery_ujs
 
 window.$ = window.jQuery = global.$ = require('jquery');
 var React = window.React = global.React = require('react');
